9e4a40957fe714a2819edb535c5acf300d23a393
bbeloin <bbeloin@student.neumont.edu>
2024-02-15 11:55:49 -0700

    Update FinalQuestController.java

--- src/controller/FinalQuestController.java
+++ src/controller/FinalQuestController.java
@@ -15,47 +15,83 @@ public class FinalQuestController {
     public void run(){
 
         boolean usingProgram = true;
+        Enemy enemy = new Enemy();
 
+        createCharacter();
 
         while (usingProgram) {
             int menuItem = ui.displayMainMenu();
 
+            if (!enemy.isAlive()){
+                System.out.println(Quest.getQuest());
+            }
+
             switch (menuItem) {
                 case 1:
-//                    displayAllZombies();
+                    acceptQuest();
                     break;
                 case 2:
-//                    int randomZombie = random.nextInt(10) + 1;
-//
-//                    for (int i = 0; i < randomZombie; i++) {
-//                        zombieCalls();
-//                    }
-//
-//                    for (int i = 0; i < randomZombie; i++) {
-//                        displayAllZombies();
-//                    }
-
+                    denyQuest();
                     break;
                 case 3:
-//                    int num = ui.numberOfZombies();
-
-//                    for (int i = 0; i < num && num <= 2147483647; i++) {
-////                        if (num <= 2147483647){
-////
-////                        }
-////                        zombieCalls();
-//                    }
-//
-//                    for (int i = 0; i < num; i++) {
-////                        displayAllZombies();
-//                    }
-
-                    break;
-                case 4:
                     usingProgram = false;
                     break;
+
+            }
+
+            String question = "What do you do? ";
+            String q1Attack = " 1. Attack: ";
+            String q2RunAway = " 2. Run Away ";
+
+            int choice = ui.getActionChoice(question, q1Attack, q2RunAway);
+
+            switch (choice){
+                case 1:
+                    attack();
+                    break;
+                case 2:
+                    runAway();
+                    break;
+            }
+
+        }
+
+    }
+
+    private Player createCharacter() {
+
+        String name = ui.getCharacterName();
+
+        String sRace = ui.getRace();
+        PlayerRaces races = getEnumIgnoreCase(sRace);
+
+        int hp = random.nextInt(10) + 1;
+        int con = random.nextInt(10) + 1;
+        int str = random.nextInt(10) + 1;
+        int dex = random.nextInt(10) + 1;
+        int ac = random.nextInt(10) + 1;
+
+        Player player = new Player(name, races, hp, con, str, dex, ac);
+
+        return player;
+    }
+
+    public static PlayerRaces getEnumIgnoreCase(String value) {
+        for (PlayerRaces races : PlayerRaces.values()) {
+            if (races.name().equalsIgnoreCase(value)) {
+                return races;
             }
         }
+        return null;
     }
+
+    private void denyQuest() {}
+
+    private void acceptQuest() {}
+
+    private void attack(){}
+
+    private void runAway(){}
+
 
 }
